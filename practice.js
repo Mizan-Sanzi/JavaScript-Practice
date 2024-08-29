ba85ca04ddf7acfd583d549c379ea2d56717af22
mizansanzi <pleasemailmizan@gmail.com>
2024-08-29 16:41:18 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,32 @@
+console.log("Hello, this is the javascript coding practice.");
+// This is a singleline comment. And the shortcut for the multiline comment is Shift+Alt+A
+/* 
+*****************************************************************************************************
+                            Math Needed for programming
+*****************************************************************************************************
+1. Number
+2. Integer (odd, even)
+3. Float (decimal number)
+4. Operations : + - * /
+5. More operations: % (modulas/reminder) []
+6. Percentage
+7. Sum
+8. Average
+9. Max/Min
+10. Series
+11. Area
+12. Some of Higher math [for advance or big projects' case]
+ */
+/* 
+*******************************************************************************************************
+                            Variable/ 5 things to declare a variable
+*******************************************************************************************************
+
+*/
+console.log("This is my birth year: 1989");
+var birthYear = 1995;
+console.log("The data", birthYear, "is:", typeof birthYear);
+var yearNow = 2024;
+console.log("And the year now is:", yearNow);
+var presentAge = yearNow-birthYear;
+console.log("So my present age is:", presentAge);
